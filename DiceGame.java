5936393c99d83a2da7580af2fa111fa0df3d1cee
s1260078aizu <s1260078@u-aizu.ac.jp>
2019-07-10 19:29:01 +0900

    add one dice

--- DiceGame.java
+++ DiceGame.java
@@ -35,14 +35,17 @@ class DiceGame{
 	Player player = new Player("John");
 	Die die1 = new Die();
 	Die die2 = new Die();
+	Die die3 = new Die();
 	int total;
 	player.greeting();
 	System.out.println("Rolling the dice...");	
 	die1.roll();
 	die2.roll();
+	die3.roll();
 	System.out.println("Die1: "+die1.getFaceValue());
 	System.out.println("Die2: "+die2.getFaceValue());
-	total=die1.getFaceValue()+die2.getFaceValue();
+       	System.out.println("Die3: "+die3.getFaceValue());
+	total=die1.getFaceValue()+die2.getFaceValue()+die3.getFaceValue();
 	System.out.println("Total value: "+total);
 	if(total>7)System.out.println(player.getName()+" won.");
 	else System.out.println(player.getName()+" lost.");
